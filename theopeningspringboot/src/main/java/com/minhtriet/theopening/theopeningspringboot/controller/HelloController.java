b680ff5faec38b1e94ada24f3fdc1d49d40264ec
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-05-28 21:28:10 +0700

    up

--- theopeningspringboot/src/main/java/com/minhtriet/theopening/theopeningspringboot/controller/HelloController.java
+++ theopeningspringboot/src/main/java/com/minhtriet/theopening/theopeningspringboot/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.minhtriet.theopening.theopeningspringboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/buoi1/hello")
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
         return "Hello THE OPENING SPRING Boot !!!";
     }
