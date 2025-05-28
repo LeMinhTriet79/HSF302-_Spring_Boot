2dd44b1def8fded1274d4eacea42996b433c97eb
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-05-28 20:40:10 +0700

    up

---
+++ theopeningspringboot/src/main/java/com/minhtriet/theopening/theopeningspringboot/TheopeningspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.minhtriet.theopening.theopeningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheopeningspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheopeningspringbootApplication.class, args);
+	}
+
+}
---
+++ theopeningspringboot/src/test/java/com/minhtriet/theopening/theopeningspringboot/TheopeningspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhtriet.theopening.theopeningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheopeningspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
