c5ba6bb670de16318b1c42d821e833f4c543957b
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-05-29 19:53:42 +0700

    up

---
+++ Demo_Du_An/smoking_swp/src/main/java/com/example/smoking_swp/SmokingSwpApplication.java
@@ -0,0 +1,13 @@
+package com.example.smoking_swp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokingSwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokingSwpApplication.class, args);
+	}
+
+}
