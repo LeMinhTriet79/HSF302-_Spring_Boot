354b9b8084b6f721cb37cb0915ff2d2c86b623b1
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-06-03 14:18:51 +0700

    up

--- Demo_SWP/demo/src/main/java/com/minhtriet/demo/controller/AuthController.java
+++ Demo_SWP/demo/src/main/java/com/minhtriet/demo/controller/AuthController.java
@@ -1,19 +1,28 @@
 package com.minhtriet.demo.controller;
 
 import com.minhtriet.demo.dto.LoginRequest;
+import com.minhtriet.demo.entity.User;
 import com.minhtriet.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000") // Cho phép React truy cập
+@CrossOrigin(origins = "http://localhost:5177") // Cho phép React truy cập
 public class AuthController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/api/user")
+    public ResponseEntity getStudent() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
--- Demo_SWP/demo/src/main/java/com/minhtriet/demo/service/UserService.java
+++ Demo_SWP/demo/src/main/java/com/minhtriet/demo/service/UserService.java
@@ -5,12 +5,17 @@ import com.minhtriet.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
     public Optional<User> login(String email, String password) {
         Optional<User> userOpt = userRepository.findByEmail(email);
