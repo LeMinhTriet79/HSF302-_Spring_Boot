dfce96eff9b1ec55ff1e1188fed201f74d20839c
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-06-03 13:23:25 +0700

    up

---
+++ Demo_SWP/demo/src/main/java/com/minhtriet/demo/controller/AuthController.java
@@ -0,0 +1,4 @@
+package com.minhtriet.demo.controller;
+
+public class AuthController {
+}
--- Demo_SWP/demo/src/main/java/com/minhtriet/demo/service/UserService.java
+++ Demo_SWP/demo/src/main/java/com/minhtriet/demo/service/UserService.java
@@ -1,4 +1,22 @@
 package com.minhtriet.demo.service;
 
+import com.minhtriet.demo.entity.User;
+import com.minhtriet.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> login(String email, String password) {
+        Optional<User> userOpt = userRepository.findByEmail(email);
+        if(userOpt.isPresent() && userOpt.get().getPassword().equals(password)) {
+            return userOpt;
+        }
+        return Optional.empty();
+    }
 }
