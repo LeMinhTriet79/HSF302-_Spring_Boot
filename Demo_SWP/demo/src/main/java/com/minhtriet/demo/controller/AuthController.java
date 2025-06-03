3e9395887be1f01973684e7b3cd5541d15a4d464
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-06-03 13:31:35 +0700

    up

--- Demo_SWP/demo/src/main/java/com/minhtriet/demo/controller/AuthController.java
+++ Demo_SWP/demo/src/main/java/com/minhtriet/demo/controller/AuthController.java
@@ -1,4 +1,25 @@
 package com.minhtriet.demo.controller;
 
+import com.minhtriet.demo.dto.LoginRequest;
+import com.minhtriet.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000") // Cho phép React truy cập
 public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        return userService.login(request.getEmail(), request.getPassword())
+                .map(user -> ResponseEntity.ok().body("Đăng nhập thành công"))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                        .body("Email hoặc mật khẩu không đúng"));
+    }
 }
