a971f2ada11366743b02c8b51cf272f738484990
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-06-16 12:00:20 +0700

    up

--- Session07-SpringMVC/java-coffee/src/main/java/org/minhtriet/coffee/coffee/CoffeeApiController.java
+++ Session07-SpringMVC/java-coffee/src/main/java/org/minhtriet/coffee/coffee/CoffeeApiController.java
@@ -1,4 +1,17 @@
 package org.minhtriet.coffee.coffee;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//là một bean, nhưng truyền, được new tự động, và vác hàm trong này dùng để map với một
+//url khi user gõ trên trình duyệt
+@RequestMapping("/api/coffees") //nếu user gọi localhost://api/coffees sữ đến class này phục vụ
 public class CoffeeApiController {
+
+    @GetMapping
+    public String getACup(){
+        return "cà phê là thứ rất quan trọng cho lập trình viên" +
+                "...Uống cà phê là pass môn";
+    }
 }
