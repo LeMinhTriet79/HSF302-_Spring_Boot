424677b716270d0ac087c144d30f919c298f087c
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-06-19 10:59:15 +0700

    up

--- Session07-SpringMVC/nt-coffee/src/main/java/org/minhtriet/coffee/ntcoffee/controller/LoginController.java
+++ Session07-SpringMVC/nt-coffee/src/main/java/org/minhtriet/coffee/ntcoffee/controller/LoginController.java
@@ -1,4 +1,19 @@
 package org.minhtriet.coffee.ntcoffee.controller;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller//bean tồn tại trong ram, tomcat, để handle, cái request url guiwrl lên
+//@RequestMapping("/login")
 public class LoginController {
+    //các hàm phục vụ nhu cầu Login và Map tới cái xẹt / , /login, post của login
+
+    //load/trả trang login cho Thymeleaf -> render -> Tomcat -> browser
+    @GetMapping( {"/", "/login"})
+    public String showLogin() {
+        return "login"; // ghép cái tên trang + .html -> login.html đưa cho Thymeleaf; gửi kèm thêm data
+        //nếu cần - Model model
+    }
 }
