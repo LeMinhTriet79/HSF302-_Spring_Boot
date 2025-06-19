abdf5eb9e85c64fc138656b9d764beb1bd79420f
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-06-19 10:33:55 +0700

    up

---
+++ Session07-SpringMVC/nt-coffee/src/main/java/org/minhtriet/coffee/ntcoffee/NtCoffeeApplication.java
@@ -0,0 +1,13 @@
+package org.minhtriet.coffee.ntcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtCoffeeApplication.class, args);
+    }
+
+}
