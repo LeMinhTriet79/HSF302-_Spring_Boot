ee719054aeea61c758bf5d2e3252be5b5f550f0e
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-06-19 11:50:07 +0700

    up

--- Session07-SpringMVC/nt-coffee/src/main/java/org/minhtriet/coffee/ntcoffee/controller/LoginController.java
+++ Session07-SpringMVC/nt-coffee/src/main/java/org/minhtriet/coffee/ntcoffee/controller/LoginController.java
@@ -1,5 +1,6 @@
 package org.minhtriet.coffee.ntcoffee.controller;
 
+import org.minhtriet.coffee.ntcoffee.entity.Product;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller//bean tồn tại trong ram, tomcat, để handle, cái request url guiwrl lên
 //@RequestMapping("/login")
@@ -22,7 +26,20 @@ public class LoginController {
 
     @PostMapping("/doLogin")
     public String doLoginAhihi(@RequestParam("user") String username, @RequestParam("pass") String password, Model model) {
+
+        //chuẩn bị sẵn danh sách món cafe, list<>, đính kèm theo thằng thùng chưa model
+        //Thùng chứa model sẽ chưa email + list
+        //
+
+
+        List<Product> productList = List.of(new Product("1", "Coffee Indonesia", 20000),
+                new Product("2", "Cà phê Ả Rập", 15000),
+                new Product("3", "Coffee Latte", 18000)
+
+        );
+
         model.addAttribute("sentUser", username);
+        model.addAttribute("products", productList);
         return "products"; //không cần chữ .html
         //cơ chế giữ nguyên url cũ của doLogin
         //resubmission, nếu F5 lại thì... submit lại from, ko an toàn
