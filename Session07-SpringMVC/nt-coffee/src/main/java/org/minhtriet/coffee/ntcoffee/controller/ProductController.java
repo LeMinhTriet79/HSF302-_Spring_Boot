e7ed12b17b818feb75e3807240c6ace845fbcb95
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-06-23 12:06:57 +0700

    UP

--- Session07-SpringMVC/nt-coffee/src/main/java/org/minhtriet/coffee/ntcoffee/controller/ProductController.java
+++ Session07-SpringMVC/nt-coffee/src/main/java/org/minhtriet/coffee/ntcoffee/controller/ProductController.java
@@ -6,11 +6,35 @@ import org.minhtriet.coffee.ntcoffee.entity.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Controller
 public class ProductController {
+
+    @GetMapping("/products/add")
+    public String addProduct(Model model) {
+        model.addAttribute("p", new Product());
+        return "product-form";
+    }
+
+
+    @GetMapping("/products/edit/{id}")
+    public String edit(Model model, @PathVariable String id) {
+        //có trong tay id số mấy rồi thì vào database query lên, ta tạm thời hardcode
+        Product product;
+        if(id.equalsIgnoreCase("1")){
+            product = new  Product("1", "Coffee Indonesia", 20000);
+        } else if (id.equalsIgnoreCase("2")) {
+            product =  new Product("2", "Cà phê Ả Rập", 15000);
+        }else {
+            product = new Product("3", "Coffee Latte", 18000);
+        }
+        model.addAttribute("p", product);
+        return "product-form";
+    }
+
     @GetMapping("/HoangXuanTrinh")
     public String list(Model model, HttpSession session) {
         List<Product> productList = List.of(new Product("1", "Coffee Indonesia", 20000),
