8a537bdd79792edea7299d07bb54ae4fdab0a2bc
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-07-14 11:16:46 +0700

    up

--- ON_THI_PE/252_ngoctrinh-v3binding/ngoctrinh-v3binding/src/main/java/com/giaolang/coffee/repository/ProductRepo.java
+++ ON_THI_PE/252_ngoctrinh-v3binding/ngoctrinh-v3binding/src/main/java/com/giaolang/coffee/repository/ProductRepo.java
@@ -3,6 +3,8 @@ package com.giaolang.coffee.repository;
 import com.giaolang.coffee.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepo extends JpaRepository<Product,String> {
 
     //mặc định JPA sẽ cung cấp cho bạn rất nhiều hàm, tự nó code luôn cho bạn
@@ -13,4 +15,8 @@ public interface ProductRepo extends JpaRepository<Product,String> {
     //BUID - IN - METHOD - SAVE(), findAll(),...
 
     //2. Hàm tự sinh ra theo cách bạn gõ tên hàm theo cú pháp
+    //derived query method
+    //Bạn cần gõ tên hàm bạn muốn tạo ở đây, không cần viết code
+    public List<Product> searchAllByNameContainingIgnoreCase(String keyword);
+    //tự sinh cái hàm select * from product where name like %keyword%
 }
