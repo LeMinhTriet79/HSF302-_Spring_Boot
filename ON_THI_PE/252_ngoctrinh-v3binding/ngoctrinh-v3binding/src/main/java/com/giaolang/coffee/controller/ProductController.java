683e28f82b52a89314012c8002969b0f407d4de5
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-07-14 10:00:02 +0700

    up

---
+++ ON_THI_PE/252_ngoctrinh-v3binding/ngoctrinh-v3binding/src/main/java/com/giaolang/coffee/controller/ProductController.java
@@ -0,0 +1,91 @@
+package com.giaolang.coffee.controller;
+
+import com.giaolang.coffee.entity.Product;
+import com.giaolang.coffee.service.CategoryService;
+import com.giaolang.coffee.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller //bean luôn
+public class ProductController {
+
+    //NHỜ SERVICE GIÚP LÁY FULL DATA TỪ TABLE
+    //TỰ SERVICE CHƠI VỚI REPO. NGUYÊN LÝ SRP
+    @Autowired
+    private ProductService productService;
+
+    @Autowired  //phục vụ cho danh sách treo đầu dê...
+    private CategoryService categoryService;
+
+    @GetMapping("/products")
+    public String showProducts(Model model) {
+
+        List<Product> productList = productService.getAllProducts();
+
+        model.addAttribute("prods", productList);
+
+        //thùng hàng đi kèm trang chứa full sản phầm từ table
+        return "products"; //.html
+    }
+
+    //link edit đc nhấn
+    @GetMapping("/products/edit/{id}")
+    public String editProduct(@PathVariable("id") String id, Model model) {
+
+        //có id rồi, where và ném thằng này về trang form - detail
+        Product product = productService.getProductById(id);
+
+        //                         Product selectedOne = product; ở trên
+        model.addAttribute("selectedOne", product);
+
+        model.addAttribute("cates", categoryService.getAllCategories());
+
+        //thùng hàng đi kèm trang chứa full sản phầm từ table
+        return "product-form"; //.html
+    }
+    //link new đc nhân
+    @GetMapping("/products/new")
+    public String newProduct(Model model) {
+
+        //constructor rỗng tạo 1 object mang giá trị default ở các field bên trong
+        //chuỗi là rỗng, số là 0, boolean là sai
+        model.addAttribute("selectedOne", new Product());
+
+        model.addAttribute("cates", categoryService.getAllCategories());
+        //thùng hàng đi kèm trang chứa full sản phầm từ table
+        return "product-form"; //.html
+    }
+
+    //button save đc nhấn
+    @PostMapping("/products/save")
+    //làm ko khéo, 1 đống @RequestParam map từng field ở form vào từng biến ở tham số hàm
+    //public String saveProduct(@RequestParam("name") String name, @Res....) {
+    public String saveProduct(@ModelAttribute("selectedOne") Product product) {
+
+        //gọi service save hoy, tạm bỏ qua key trùng, validation
+        productService.saveProduct(product);
+
+        return "redirect:/products"; //url nhen, ko phải tên trang!!!
+    }      //gọi url mới, đổi url trên browser, tránh resubmission
+
+    //link delete đc nhấn
+    //link edit đc nhấn
+    @GetMapping("/products/delete/{id}")
+    public String deleteProduct(@PathVariable("id") String id, Model model) {
+
+        productService.deleteProductById(id);
+
+        //thùng hàng đi kèm trang chứa full sản phầm từ table
+        return "redirect:/products"; //.html
+    }
+    //link n
+    //link search đc nhấn
+
+}
---
+++ ON_THI_PE/252_ngoctrinh-v3binding/ngoctrinh-v3binding/src/main/java/com/giaolang/coffee/entity/Product.java
@@ -0,0 +1,90 @@
+package com.giaolang.coffee.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "Product")
+public class Product {
+
+    @Id
+    @Column(name = "Id")
+    private String id;
+
+    @Column(name = "Name", columnDefinition = "NVARCHAR(50)", nullable = false)
+    @NotBlank // 
+    private String name;
+
+    @Column(name = "Quantity", nullable = false)
+    private int quantity;
+
+    @Column(name = "Price", nullable = false)
+    private double price;
+
+    //RELATIONSHIP MANY TO ONE - NHIỀU SẢN PHẨM CÓ THỂ THUỘC 1 CATEGORY
+    //                           1 CATEGORY CÓ THỂ CÓ NHIỀU SẢN PHẨM
+    @ManyToOne
+    @JoinColumn(name = "CateId")  //cột khoá ngoại, phần DB
+    private Category cate;        //phần OOP
+
+    public Category getCate() {
+        return cate;
+    }
+
+    public void setCate(Category cate) {
+        this.cate = cate;
+    }
+
+
+    public Product() {
+    }
+
+    public Product(String id, String name, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
+}
