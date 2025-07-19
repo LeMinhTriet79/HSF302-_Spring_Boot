e7d8f79cc82aa0a54c97478a5ce9f2e513fdb3b7
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-07-19 09:26:26 +0700

    up

---
+++ Phan_On_Tap_PE/ngoctrinh-coffee-v4final/src/test/java/com/leminhtriet/coffe/NgoctrinhCoffeeV4finalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leminhtriet.coffe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoctrinhCoffeeV4finalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
